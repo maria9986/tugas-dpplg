f645f530553ccac5dbe497527140f253c5ded0d6
maria9986 <Ivonnemaria999@gmail.com>
2025-06-11 17:53:02 +0700

    Add files via upload

---
+++ belajar_dasar2.php
@@ -0,0 +1,13 @@
+   <?php 
+   $hari ="kamis";
+   $waktu = "siang";
+
+   if ($hari =="selasa"ll $hari =="jumat"ll $hari =="minggu")
+    if ($jam =="pagi"){
+        echo "menu $hari $jam adalah mie gacoan dan es teh"
+    }else if($jam =="siang"){
+        echo"menu $hari $jam adalah mie ayam dan jeruk"
+    }else {
+        echo "menu $hari $jam adalah soto dan kopi hangat"
+    }
+else if($hari =="kamis")
