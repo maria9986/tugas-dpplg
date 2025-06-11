fa8a2f125ab3ee4de3cffab903c105974eaa2ae3
maria9986 <Ivonnemaria999@gmail.com>
2025-06-11 18:09:28 +0700

    Add files via upload

---
+++ belajar_dasar3.php
@@ -0,0 +1,24 @@
+><!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jadwal Harian Andi</title>
+</head>
+<body>
+    <h1>Jadwal Harian Andi</h1>
+    <ul>
+        <li>15:30 - 15:45: Pulang sekolah</li>
+        <li>15:45 - 16:15: Mandi</li>
+        <li>16:15 - 16:45: Mengaji</li>
+        <li>16:45 - 18:45: Mengerjakan tugas sekolah</li>
+        <li>18:45 - 19:15: Menghafal dialog</li>
+        <li>19:15 - 19:30: Membeli bumbu masakan</li>
+        <li>19:30 - 20:00: Makan malam</li>
+        <li>20:00: Chatting dengan Raya (di Arab: 00:00)</li>
+        <li>21:00: Mengobrol bersama keluarga</li>
+        <li>21:30 - 22:00: Waktu luang (jika tidak ada tugas)</li>
+        <li>22:00: Tidur</li>
+    </ul>
+</body>
+</html>
